85e5ac3b53a6f315e3c8d16e54f3f9cf740bc5b1
Đỗ Phúc Danh <pepsipro159357@gmail.com>
2026-04-20 13:51:44 +0700

    Create ex6.sql

---
+++ ex6.sql
@@ -0,0 +1,19 @@
+
+CREATE DATABASE LibraryDB;
+
+CREATE SCHEMA library;
+
+CREATE TABLE library.Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    published_year INT,
+    available BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE library.Members (
+    member_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    join_date DATE DEFAULT CURRENT_DATE
+);
